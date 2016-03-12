628cf474fd8b8f1aa5fce4681093799237db2dd1
masonbrothers <masonbrothers@users.noreply.github.com>
2016-03-12 15:04:51 -0700

    Made server so it can accept date and find delay. Also changed port to 5005 instead of 8080.

--- server.js
+++ server.js
@@ -3,17 +3,65 @@ var fs = require('fs');
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var portNo = 8080;
+var portNo = 5005;
+var kill = "alive";
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+
+process.stdin.on('data', function (text){
+	var user_input = util.inspect(text);
+	console.log('user_input: ' + user_input);
+	if (text === 'quit\n') {
+		done();
+	}
+});
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', express.static(__dirname+'/'));
 
 app.post('/server_input',function(req,res){
-  console.log(req.body);
-  res.send("Server " + new Date() + req.body.toString());
-  res.end("yes");
+	console.log(req.body);
+	console.log(JSON.stringify(req.body));
+	plane_date = new Date(req.body.date);
+	console.log(plane_date);
+	server_date = new Date();
+
+	delay = server_date.getTime()- plane_date.getTime();
+	console.log(delay);
+	if (kill == "kill")
+	{
+		console.log("kill toggled. kill sent to plane.");
+	}
+	else
+	{
+		console.log("kill not toggled.");
+	}
+	res.send(kill);
+	res.end("yes");
 });
+
+/*
+
+	console.log(req.body);
+	console.log(JSON.stringify(req.body));
+
+	//new Date();
+	if (kill == "kill")
+	{
+		console.log("kill toggled. kill sent to plane.");
+	}
+	else
+	{
+		console.log("kill not toggled.");
+	}
+	res.send(kill);
+	res.end("yes");
+*/
 
 app.listen(portNo, function() { 
 	console.log('Check Port ' + portNo + '....');
