35bbd5a426ce4073e7df39a983f7987439f639e5
masonbrothers <masonbrothers@users.noreply.github.com>
2016-03-18 20:10:05 -0600

    Added wildcard accept of get and post

--- server.js
+++ server.js
@@ -24,6 +24,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', express.static(__dirname+'/'));
 
+app.all('*', function(req,res){
+	console.log(req.body);
+	console.log(JSON.stringify(req.body));
+	plane_date = new Date(req.body.date);
+	console.log(plane_date);
+	server_date = new Date();
+
+	delay = server_date.getTime()- plane_date.getTime();
+	console.log(delay);
+	if (kill == "kill")
+	{
+		console.log("kill toggled. kill sent to plane.");
+	}
+	else
+	{
+		console.log("kill not toggled.");
+	}
+	res.send(kill);
+	res.end("yes");
+});
+
 app.post('/server_input',function(req,res){
 	console.log(req.body);
 	console.log(JSON.stringify(req.body));
